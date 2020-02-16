76a48a3bfb9d743df9bdf7cb8fb2d157b5221f17
kekich <kek@cheburek>
2020-02-16 14:11:33 +0200

    Echo testing

--- script1.sh
+++ script1.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 branchtag=$($BRANCH_NAME | cut -f2 -d/)
+echo "$branchtag"
 lastdigit=$(git tag -l | grep $branchtag | tail -n 1 | cut -f3 -d.)
+echo "$lastdigit"
 newdigit=$((lastdigit+1))
 
 $(git tag -a "${branchtag}.${newdigit}" -m "Release tag ${branchtag}.${newdigit}")
