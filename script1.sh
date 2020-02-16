83e6e11a516e4fcc68db4156fec7b4e08c16c07e
kekich <kek@cheburek>
2020-02-16 12:59:49 +0200

    Fix script

--- script1.sh
+++ script1.sh
@@ -4,7 +4,7 @@ branchtag=$($BRANCH_NAME | cut -f2 -d/)
 lastdigit=$(git tag -l | grep $branchtag | tail -n 1 | cut -f3 -d.)
 newdigit=$((lastdigit+1))
 
-$(git tag -a "${branchtag}"."${newdigit}" -m "Release tag ${branchtag}"."${newdigit}")
+$(git tag -a "${branchtag}.${newdigit}" -m "Release tag ${branchtag}.${newdigit}")
 $(mvn versions:set -DnewVersion=${branchtag}"."${newdigit})
 $(mvn deploy -s /settings.xml)
 $(git push --tags)
