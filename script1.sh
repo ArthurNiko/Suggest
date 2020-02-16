79dded6e5e508ce6afa7fba40068f98097aa047c
kekich <kek@cheburek>
2020-02-16 15:36:44 +0200

    Final touch2

--- script1.sh
+++ script1.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
 branchtag=$(echo ${BRANCH_NAME} | cut -f2 -d/)
-echo "$BRANCH_NAME fdsfdsfsd"
-echo "${branchtag}"
 lastdigit=$(git tag -l | grep ${branchtag} | tail -n 1 | cut -f3 -d.)
-echo "${lastdigit}"
 newdigit=$((lastdigit+1))
 
 git tag -a "${branchtag}.${newdigit}" -m "Release tag ${branchtag}.${newdigit}"
