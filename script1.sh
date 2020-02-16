ebd4b0491c13868828397a5dd69bee80e9da2d0d
kekich <kek@cheburek>
2020-02-16 15:09:21 +0200

    Script fix

--- script1.sh
+++ script1.sh
@@ -7,7 +7,7 @@ lastdigit=$(git tag -l | grep ${branchtag} | tail -n 1 | cut -f3 -d.)
 echo "${lastdigit}"
 newdigit=$((lastdigit+1))
 
-$(git tag -a "${branchtag}.${newdigit}" -m "Release tag ${branchtag}.${newdigit}")
-$(mvn versions:set -DnewVersion=${branchtag}"."${newdigit})
-$(mvn deploy -s /settings.xml)
-$(git push --tags)
+git tag -a "${branchtag}.${newdigit}" -m "Release tag ${branchtag}.${newdigit}"
+mvn versions:set -DnewVersion=${branchtag}"."${newdigit}
+mvn deploy -s /settings.xml
+git push --tags
